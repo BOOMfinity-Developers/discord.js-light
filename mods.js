3d9eedf8564523631ccd8d3ab3cbb00d96a34d08
VenomPCPL <venompcpl@gmail.com>
2021-05-24 20:56:47 +0200

    fix(Message): Restore delete function options

--- mods.js
+++ mods.js
@@ -8,3 +8,17 @@ Discord.MessageReaction.prototype.fetch = async function() {
 	this._patch(existing || { count: 0 });
 	return this;
 }
+
+Discord.Message.prototype.delete = async function(options = {}) {
+	if (typeof options !== 'object') return Promise.reject(new TypeError('INVALID_TYPE', 'options', 'object', true));
+	const { timeout = 0 } = options;
+	if (timeout <= 0) {
+		return this.channel.messages.delete(this.id).then(() => this);
+	} else {
+		return new Promise(resolve => {
+			this.client.setTimeout(() => {
+				resolve(this.delete());
+			}, timeout);
+		});
+	}
+}
